2d2e755cfb50af16653e780f932cce8e2837e624
Rob Figueiredo <robfig@gmail.com>
2011-03-20 16:28:39 -0400

    Show a nice compilation error message if inline coffee is incorrect.

--- app/tags/CoffeeTags.java
+++ app/tags/CoffeeTags.java
@@ -1,22 +1,19 @@
 package tags;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.io.PrintWriter;
-import java.io.PrintStream;
-
-import org.jcoffeescript.JCoffeeScriptCompiler;
 import org.jcoffeescript.JCoffeeScriptCompileException;
+import org.jcoffeescript.JCoffeeScriptCompiler;
 
 import groovy.lang.Closure;
-import play.PlayPlugin;
-import play.templates.FastTags;
-import play.templates.JavaExtensions;
+
+import java.io.PrintWriter;
+import java.util.Map;
+
+import play.exceptions.TemplateExecutionException;
 import play.templates.FastTags.Namespace;
-import play.templates.Template;
-import play.vfs.VirtualFile;
+import play.templates.FastTags;
 import play.templates.GroovyTemplate.ExecutableTemplate;
+import play.templates.JavaExtensions;
+import play.templates.Template;
 
 /**
  * This file has Tag support for Coffee.
@@ -36,8 +33,14 @@ public class CoffeeTags extends FastTags {
             out.print(js);
             out.print("</script>");
         } catch (JCoffeeScriptCompileException e) {
-            play.Logger.error("Error: " + e);
-            // TODO: Show a nice compilation error.
+            // Show a nice compilation error message.
+            // All of the exceptions say "(Type of error) on line 54: (Error detail)"
+            play.Logger.error(e, "Coffee compilation error");
+            String error = e.getMessage();
+            int i = error.indexOf("line ");
+            int coffeeLine = Integer.parseInt(error.substring(i + 5, error.indexOf(":", i)));
+            throw new TemplateExecutionException(
+                template.template, fromLine + coffeeLine, e.getMessage(), e);
         }
     }
 }
